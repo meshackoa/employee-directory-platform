a015583d87ae5ba4f61f8541c60dbcce025f0408
meshackoa <meshackoa@yahoo.com>
2026-07-18 01:48:22 -0400

    Update Terraform backend for personal AWS account

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket       = "landmark-terraform-state-file"
+    bucket       = "employee-platform-terraform-state-381155823715"
     key          = "eks/terraform.tfstate"
     region       = "us-east-1"
     profile      = "terraform"
